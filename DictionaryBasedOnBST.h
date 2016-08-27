e0329dd80b39fa5f72738f49c24dc9e5d6bc8e69
dakodako <chididi_gloria@hotmail.com>
2016-08-28 01:58:24 +1000

    insertion completed

--- DictionaryBasedOnBST.h
+++ DictionaryBasedOnBST.h
@@ -1,15 +1,25 @@
+#ifndef inputnoutput_h
+#define inputnoutput_h
+
+#include <stdio.h>
 #include "Entry.h"
 struct node
 {
-	struct node *left; /*left child of the node*/
-	struct node *right; /* right child of the node*/
-	struct node *parent; // still thinking whether should add parent node or not
-	dict_t data;
+    struct node *left; /*left child of the node*/
+    struct node *right; /* right child of the node*/
+    struct node *parent; // still thinking whether should add parent node or not
+    dict_t *data;
 };
 
 typedef struct node node_t;
-typedt node_t *tree; // the root of the tree
+typedef node_t *tree; // the root of the tree
 
 // functions
 
 tree insert(tree T, dict_t *data);
+tree insert_numof_changes(tree T, dict_t *data,int *numOfChanges);
+
+// test functions
+
+void inOrderTreeWalk(tree T);
+#endif /* inputnoutput_h */
