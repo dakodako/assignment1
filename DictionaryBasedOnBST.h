df38423bc9b08b58d381ed4333c05d68d06c3469
dakodako <chididi_gloria@hotmail.com>
2016-08-28 15:32:01 +1000

    locating duplicate keys

--- DictionaryBasedOnBST.h
+++ DictionaryBasedOnBST.h
@@ -2,11 +2,12 @@
 #define inputnoutput_h
 
 #include <stdio.h>
-#include "Entry.h"
+//#include "Entry.h"
+#include "List.h"
 struct node
 {
-    struct node *left; /*left child of the node*/
-    struct node *right; /* right child of the node*/
+    struct node *left; //left child of the node
+    struct node *right; // right child of the node
     struct node *parent; // still thinking whether should add parent node or not
     dict_t *data;
 };
@@ -20,7 +21,7 @@ tree insert(tree T, dict_t *data);
 tree insert_numof_changes(tree T, dict_t *data,int *numOfChanges);
 dict_t* tree_search(node_t *x, char *k);
 node_t* iterative_tree_search(node_t *x, char *k);
-
+list tree_search_locate_duplicate(node_t*, char *k);
 // test functions
 
 void inOrderTreeWalk(tree T);
