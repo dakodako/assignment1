005b74252bb5466453b606d0bba4aa76c086a275
dakodako <chididi_gloria@hotmail.com>
2016-08-28 15:33:20 +1000

    empty

---
+++ inputnoutput.h
@@ -0,0 +1,14 @@
+//
+//  inputnoutput.h
+//  Assignment1
+//
+//  Created by Didi Chi on 27/08/2016.
+//  Copyright © 2016 dako. All rights reserved.
+//
+
+#ifndef inputnoutput_h
+#define inputnoutput_h
+
+#include <stdio.h>
+
+#endif /* inputnoutput_h */
