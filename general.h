05e4852703057d9933080123de8a04dbeffc6cfd
dakodako <chididi_gloria@hotmail.com>
2016-08-29 15:28:44 +1000

    implementing general functions (functions are irrelevent with data structures)

---
+++ general.h
@@ -0,0 +1,16 @@
+//
+//  general.h
+//  Assignment1
+//
+//  Created by Didi Chi on 29/08/2016.
+//  Copyright © 2016 dako. All rights reserved.
+//
+
+#ifndef general_h
+#define general_h
+
+#include <stdio.h>
+
+char* char_malloc_with_error_handling(size_t s);
+
+#endif /* general_h */
