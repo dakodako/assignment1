1afc94882ee6279775b4b40f866f11970c66e9a0
dakodako <chididi_gloria@hotmail.com>
2016-08-29 16:03:08 +1000

    Delete Dictionary.h

--- Dictionary.h
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-struct dict
-{
-	char *key; /* in yelp database, the key will be <name>*/
-	char *value; /* in yelp database, the value will be <data>*/
-};
-typedef 
