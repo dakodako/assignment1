1a8104d07aab833dbfc9d591817ee513b7b1dc0b
dakodako <chididi_gloria@hotmail.com>
2016-08-31 05:17:39 +1000

    output files working

--- List.c
+++ List.c
@@ -9,16 +9,16 @@
 #include "List.h"
 // Given an dictionary entry value, the list_insert procedure splices value onto the front of the linked list
 list list_insert(list L, dict_t* value){
-    printf("insert happening\n");
+    //printf("insert happening\n");
     
     list_node_t *newListNode = (list_node_t*)malloc(sizeof(newListNode));
     newListNode -> key = value;
     if(L == NULL){
-        printf("starts to build a list\n");
+      //  printf("starts to build a list\n");
         L = newListNode;
         L ->next = NULL;
     } else {
-        printf("add a node\n");
+        //printf("add a node\n");
         newListNode->next = L;
         L = newListNode;
     }
@@ -37,3 +37,39 @@ void print_list(list L){
         currentNode = currentNode ->next;
     }
 }
+void deleteAllNodes(list L){
+    
+    while(L != NULL){
+        list_node_t *temp = L;
+        free(temp);
+        L = L ->next;
+    }
+}
+
+void fprint_list(list L, FILE* myFile,char*search){
+    list_node_t *currentNode = L;
+    if(currentNode == NULL){
+        fprintf(myFile, "%s",search);
+        fprintf(myFile, " -> ");
+        fprintf(myFile, "NOTFOUND\n");
+        return;
+    }
+    while(currentNode != NULL){
+        //printf("writing\n");
+        fprintf(myFile, "%s",currentNode->key->key);
+        fprintf(myFile, " -> ");
+        fprintf(myFile, "%s",currentNode->key->value);
+        currentNode = currentNode -> next;
+    }
+}
+
+/*void deleteAllNodes(node* start)
+{
+    while (start != null)
+    {
+        node* temp = start;
+        free(temp);
+        start = start -> next;
+    }
+}*/
+
